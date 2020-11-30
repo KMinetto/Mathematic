73cb4e960702a83cd432af0406c3f274529c73aa
KMinetto <killian.minetto@gmail.com>
2020-11-30 16:26:52 +0100

    Add revision page

--- assets/js/revision.js
+++ assets/js/revision.js
@@ -1,33 +1,43 @@
+/** Import **/
+
+import { addOperation } from "./functions.js";
+
 /** Récupération des Tags HTML **/
 
 const select = document.querySelector('#selectTable');
 const btn = document.querySelector('#revisionBtn');
 const form = document.querySelector('#revision');
+const tbody = document.querySelector('tbody');
 
 /** Algorithme **/
 
 /** revision.html **/
 for (let i = 1; i <= 10; i++) {
     const value = i;
-    const label = `table de ${ i }`;
+    const label = `table de ${i}`;
 
     select.insertAdjacentHTML('beforeend',
-        `<option name="selectOption" value="${ value }">${ label }</option>`
+        `<option value="${value}">${label}</option>`
     );
 }
-//
-// btn.addEventListener('click', (e) => {
-//     e.preventDefault();
-//
-//     const formData = new FormData(form);
-//
-//     fetch('assets/php/revision.php', {
-//         method: 'POST',
-//         body: formData,
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data);
-//         })
-//         .catch(err => console.error(err));
-// });
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    fetch('assets/php/revision.php', {
+        method: 'POST',
+        body: formData,
+    })
+        .then(response => response.json())
+        .then(data => {
+            tbody.innerHTML = "";
+            for (let tableRow in data) {
+                console.log(data[tableRow]);
+                tbody.appendChild(addOperation(data[tableRow]['operation'], data[tableRow]['result']));
+            }
+        })
+        .catch(err => console.error(err));
+});
+
