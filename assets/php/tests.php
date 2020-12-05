691ed19b146af1593981d70dc23a0a535270280e
KMinetto <killian.minetto@gmail.com>
2020-12-05 02:11:18 +0100

    add visual to test page

--- assets/php/tests.php
+++ assets/php/tests.php
@@ -1,6 +1,12 @@
 <?php
 
-if (!empty($_POST)) {
+//if (!empty($_POST) && isset($_POST['submit1'])) {
+//    $x = 'Bonjour tout le monde';
+//
+//    try {
+//        echo json_encode($x, JSON_THROW_ON_ERROR);
+//    } catch (JsonException $e) {
+//        $e->getMessage();
+//    }
+//}
 
-
-}
